a4d7d87140caac14e1a3371de3382149a8efeb59
Mariglen Doda <mariglendoda1999@gmail.com>
2023-12-15 13:39:31 +0100

    Structuring files

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
+app.get('/', function (request, response) {
   response.send('<h1>My first express app</h1>');
 });
 
+app.get('/restaurants', function (request, response) {
+
+});
 
 app.listen(3000, () => console.log('Listening on port 3000'));
+
+
 
 
