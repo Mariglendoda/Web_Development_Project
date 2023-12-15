f7a2b8f7eda7aa5e1747eea369ae9e4656144d1a
Mariglen Doda <mariglendoda1999@gmail.com>
2023-12-15 23:58:57 +0100

    Adding the ejs templates engine

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
+app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({extended: false}));
 
 // Home page
