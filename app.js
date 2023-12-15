8613b43a58c8d960f9c367aae38b449ebd7fdecf
Mariglen Doda <mariglendoda1999@gmail.com>
2023-12-15 13:54:22 +0100

    Implementing all route path for all html files

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 
 const app = express();
@@ -7,7 +9,23 @@ app.get('/', function (request, response) {
 });
 
 app.get('/restaurants', function (request, response) {
+  const htmlPath = path.join(__dirname, 'views', 'restaurants.html');
+  response.sendFile(htmlPath);
+});
 
+app.get('/recommend', function (request, response) {
+  const htmlPath = path.join(__dirname, 'views', 'recommend.html');
+  response.sendFile(htmlPath);
+});
+
+app.get('/confirm', function (request, response) {
+  const htmlPath = path.join(__dirname, 'views', 'confirm.html');
+  response.sendFile(htmlPath);
+});
+
+app.get('/about', function (request, response) {
+  const htmlPath = path.join(__dirname, 'views', 'about.html');
+  response.sendFile(htmlPath);
 });
 
 app.listen(3000, () => console.log('Listening on port 3000'));
