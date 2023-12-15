a666bedb35b569f7800262abc9757a556bceb602
Mariglen Doda <mariglendoda1999@gmail.com>
2023-12-15 16:45:09 +0100

    Adding comments for different roues

--- app.js
+++ app.js
@@ -6,26 +6,36 @@ const app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+
+// Home page
 app.get('/', function (request, response) {
   const htmlPath = path.join(__dirname, 'views', 'index.html');
   response.sendFile(htmlPath);
 });
 
+
+// Restaurants page
 app.get('/restaurants', function (request, response) {
   const htmlPath = path.join(__dirname, 'views', 'restaurants.html');
   response.sendFile(htmlPath);
 });
 
+
+// Recommendations page
 app.get('/recommend', function (request, response) {
   const htmlPath = path.join(__dirname, 'views', 'recommend.html');
   response.sendFile(htmlPath);
 });
 
+
+// Confirmation page
 app.get('/confirm', function (request, response) {
   const htmlPath = path.join(__dirname, 'views', 'confirm.html');
   response.sendFile(htmlPath);
 });
 
+
+// About page
 app.get('/about', function (request, response) {
   const htmlPath = path.join(__dirname, 'views', 'about.html');
   response.sendFile(htmlPath);
